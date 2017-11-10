fb7e6bb47f6dadf1ae97a83edb774e164c4c52b4
Cody Seibert <cseibert113@gmail.com>
2017-11-09 21:26:03 -0500

    initial setup

---
+++ main.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MINESWEEPER</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  
+  <body>
+    <h1>MINESWEEPER</h1>
+    <div id="board"></div>
+    <script src="main.js"></script>
+  </body>
+</html>

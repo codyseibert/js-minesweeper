26526a46becc5c40399b4744b3a6294529690125
Cody Seibert <cseibert113@gmail.com>
2017-11-09 22:27:39 -0500

    final project

--- main.js
+++ main.js
@@ -1,16 +1,119 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>MINESWEEPER</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  </head>
+const $board = $('#board');
+const ROWS = 10;
+const COLS = 10;
+
+function createBoard(rows, cols) {
+  $board.empty();
+  for (let i = 0; i < rows; i++) {
+    const $row = $('<div>').addClass('row');
+    for (let j = 0; j < cols; j++) {
+      const $col = $('<div>')
+        .addClass('col hidden')
+        .attr('data-row', i)
+        .attr('data-col', j);
+      if (Math.random() < 0.1) {
+        $col.addClass('mine');
+      }
+      $row.append($col);
+    }
+    $board.append($row);
+  }
+}
+
+function restart() {
+  createBoard(ROWS, COLS);
+}
+
+function gameOver(isWin) {
+  let message = null;
+  let icon = null;
+  if (isWin) {
+    message = 'YOU WON!';
+    icon = 'fa fa-flag';
+  } else {
+    message = 'YOU LOST!';
+    icon = 'fa fa-bomb';
+  }
+  $('.col.mine').append(
+    $('<i>').addClass(icon)
+  );
+  $('.col:not(.mine)')
+    .html(function() {
+      const $cell = $(this);
+      const count = getMineCount(
+        $cell.data('row'),
+        $cell.data('col'),
+      );
+      return count === 0 ? '' : count;
+    })
+  $('.col.hidden').removeClass('hidden');
+  setTimeout(function() {
+    alert(message);
+    restart();
+  }, 1000);
+}
+
+function reveal(oi, oj) {
+  const seen = {};
+
+  function helper(i, j) {
+    if (i >= ROWS || j >= COLS || i < 0 || j < 0) return;
+    const key = `${i} ${j}`
+    if (seen[key]) return;
+    const $cell =
+      $(`.col.hidden[data-row=${i}][data-col=${j}]`);
+    const mineCount = getMineCount(i, j);
+    if (
+      !$cell.hasClass('hidden') ||
+      $cell.hasClass('mine')
+    ) {
+      return;
+    }
+
+    $cell.removeClass('hidden');
+
+    if (mineCount) {
+      $cell.text(mineCount);
+      return;
+    }
+    
+    for (let di = -1; di <= 1; di++) {
+      for (let dj = -1; dj <= 1; dj++) {
+        helper(i + di, j + dj);
+      }      
+    }
+  }
+
+  helper(oi, oj);
+}
+
+function getMineCount(i, j) {
+  let count = 0;
+  for (let di = -1; di <= 1; di++) {
+    for (let dj = -1; dj <= 1; dj++) {
+      const ni = i + di;
+      const nj = j + dj;
+      if (ni >= ROWS || nj >= COLS || nj < 0 || ni < 0) continue;
+      const $cell =
+        $(`.col.hidden[data-row=${ni}][data-col=${nj}]`);
+      if ($cell.hasClass('mine')) count++;
+    }      
+  }
+  return count;
+}
+
+$board.on('click', '.col.hidden', function() {
+  const $cell = $(this);
+  const row = $cell.data('row');
+  const col = $cell.data('col');
   
-  <body>
-    <h1>MINESWEEPER</h1>
-    <div id="board"></div>
-    <script src="main.js"></script>
-  </body>
-</html>
+  if ($cell.hasClass('mine')) {
+    gameOver(false);
+  } else {
+    reveal(row, col);
+    const isGameOver = $('.col.hidden').length === $('.col.mine').length
+    if (isGameOver) gameOver(true);
+  }
+})
+
+restart();
